ab09601a63decfd2804397179188670c7b85e0a6
Kellams Connor <166428177+Maxwellspring@users.noreply.github.com>
2025-01-22 09:50:39 -0800

    xzZX
    ZX

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+let myVariable = "Bob";
+
+/*
+Everything in between is a comment.
+*/
+
+// This is a comment
+
+
+// the () for a function is an argument, or input
+// you also need a return part to get the output
+
+//events https://developer.mozilla.org/en-US/docs/Learn_web_development/Getting_started/Your_first_website/Adding_interactivity?authuser=0
